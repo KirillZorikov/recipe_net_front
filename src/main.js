2b6589b9e987567dda9059fe03f802207c4956e3
kirill <k.zorikov@yandex.ru>
2021-06-07 01:15:02 +0500

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
 import { createApp } from 'vue'
+import {store} from './store'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+axios.interceptors.response.use((response) => {
+        return response;
+    }, (error) => {
+        if (error.response.status !== 401) {
+            return new Promise((resolve, reject) => {
+                reject(error);
+            });
+        } else {
+            localStorage.removeItem('user');
+            return new Promise((resolve, reject) => {
+                reject(error);
+            });
+        }
+    }
+)
+
+app.use(router)
+app.use(store)
+app.use(VueAxios, axios)
+
+app.mount('#app')
